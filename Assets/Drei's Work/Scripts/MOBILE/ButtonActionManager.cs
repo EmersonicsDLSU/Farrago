2a82f409389afaef5a5b1e26eb01e77ca5a16356
Aldrey Gaurana <gauranadrei@yahoo.com>
2022-07-02 16:19:33 +0800

    fixes mobile

--- Assets/Drei's Work/Scripts/MOBILE/ButtonActionManager.cs
+++ Assets/Drei's Work/Scripts/MOBILE/ButtonActionManager.cs
@@ -8,13 +8,13 @@ public class ButtonActionManager : MonoBehaviour
 {
     public static ButtonActionManager Instance;
 
-    [HideInInspector] public Button interactButton;
-    [HideInInspector] public Button jumpButton;
-    [HideInInspector] public Button runButton;
-    [HideInInspector] public Button sneakButton;
-    [HideInInspector] public Button cleanseButton;
-    [HideInInspector] public Button journalButton;
-    [HideInInspector] public Button pauseButton;
+    public Button interactButton;
+    public Button jumpButton;
+    public Button runButton;
+    public Button sneakButton;
+    public Button cleanseButton;
+    public Button journalButton;
+    public Button pauseButton;
 
     private GameObject lastPressedButton;
     [HideInInspector] public bool isInteractHeldDown = false;
@@ -35,20 +35,13 @@ public class ButtonActionManager : MonoBehaviour
         }
         Instance = this;
 
-        DontDestroyOnLoad(this);
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        interactButton = GameObject.Find("AbsorbIntHelp").GetComponent<Button>();
-        jumpButton = GameObject.Find("JumpHelp").GetComponent<Button>();
-        runButton = GameObject.Find("RunHelp").GetComponent<Button>();
-        sneakButton = GameObject.Find("CrouchHelp").GetComponent<Button>();
-        cleanseButton = GameObject.Find("CleanseHelp").GetComponent<Button>();
-        journalButton = GameObject.Find("JournalHelp").GetComponent<Button>();
-        pauseButton = GameObject.Find("PauseButton (1)").GetComponent<Button>();
+        
     }
 
     // Update is called once per frame
