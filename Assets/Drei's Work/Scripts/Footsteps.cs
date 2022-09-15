f32f4af5fcf7b909ed13a898be73618932991b50
EmersonicsDLSU <emerson_celestial@dlsu.edu.ph>
2022-09-16 07:24:00 +0800

    Remove unnecessary Light reflections

--- Assets/Drei's Work/Scripts/Footsteps.cs
+++ Assets/Drei's Work/Scripts/Footsteps.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Footsteps : MonoBehaviour
@@ -17,16 +18,5 @@ public class Footsteps : MonoBehaviour
     {
         audioSource.PlayOneShot(clip);
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
